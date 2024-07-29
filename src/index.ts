4ee6e21329318fbf65687d08f771ce9c783edbd6
felippemozer <felippemozer22@gmail.com>
2024-07-29 13:03:55 -0300

    docs: create swagger file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import routes from "./routes";
 import swaggerUi from "swagger-ui-express";
-// import swaggerDoc from "./swagger.json";
+import swaggerDoc from "./swagger.json";
 
 export const app = express();
 
 app.use(express.json());
 app.use("/", routes);
-// app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(4000, () => {
     console.info(`Server running on port 4000`);
-})
+});
