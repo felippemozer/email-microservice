7698c5a9311f27a002cf495a2ff38c156da7efc1
felippemozer <felippemozer22@gmail.com>
2024-07-29 10:23:36 -0300

    chore: edit eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["src/**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   ...tseslint.configs.recommended,
 ];
