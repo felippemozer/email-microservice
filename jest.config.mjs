d227d311f5e65ae74208df8e443c4b4e1ed59b14
felippemozer <felippemozer22@gmail.com>
2024-07-31 11:37:21 -0300

    test: update jest config and tests to solve coverage error

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,10 +1,25 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
+  preset: "ts-jest",
+  moduleFileExtensions: [
+    "js",
+    "ts"
+  ],
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.(ts|tsx)$": ["ts-jest",{}],
   },
+  testMatch: [
+    '**/?(*.)+(spec|test).ts?(x)',
+  ],
   testPathIgnorePatterns: [
     "dist"
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.test.ts"
+  ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
   ]
 };
--- src/services/email.service.test.ts
+++ src/services/email.service.test.ts
@@ -2,17 +2,17 @@ import "dotenv/config";
 import { EmailService } from "./email.service";
 
 describe("send email", () => {
-    const emailService = new EmailService();
-
     it("should send email to account owner", () => {
+        const emailService = new EmailService();
         expect(emailService.send({
             to: "harafih372@fuzitea.com",
             subject: "Teste de envio para dono da conta",
             body: "Enviando um email para o dono da conta do serviço"
         })).resolves.toBeUndefined()
     });
-
+    
     it("should return error on send to another account other than account owner", () => {
+        const emailService = new EmailService();
         expect(emailService.send({
             to: "anothermail@mail.com",
             subject: "Teste de erro para envio para outro usuário",
